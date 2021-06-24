e1da7b9f7fbf0f0625924c694e7200981adb30a2
S M Samnoon Abrar <36998653+samnoon1971@users.noreply.github.com>
2021-06-24 12:57:47 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const puppeteer = require('puppeteer');
 
 (async() => {
-    const browswer = await puppeteer.launch();
-    const page = await browswer.newPage();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
     await page.goto('https://codeforces.com');
     await page.screenshot({path: 'cf.png'});
     await browswer.close();
